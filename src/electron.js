22ee374986f18d27d0abb70ab5a0a42f6d81fcea
Roe <88600811+roe69@users.noreply.github.com>
2024-08-09 17:14:50 +1000

    fix darwin not exiting on all windows closed

--- src/electron.js
+++ src/electron.js
@@ -74,7 +74,7 @@ app.whenReady().then(() => {
 });
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
+  app.quit();
 });
 
 ipcMain.on('downloadAndUpdate', () => {
